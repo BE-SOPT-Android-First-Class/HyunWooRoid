09e021eb792794b3e99af74d93f33d29ce44f9d0
Nunu Lee <nunu.lee@mathpresso.com>
2021-04-21 19:00:18 +0900

    #11 [CHORE] : Dagger gradle dependency setting

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -37,8 +37,13 @@ object MaterialDesignDependencies {
 object KaptDependencies {
     const val hilt = "com.google.dagger:hilt-compiler:${Versions.hilt_version}"
     const val glideCompiler = "com.github.bumptech.glide:compiler:${Versions.glide_compiler_version}"
+    const val dagger = "com.google.dagger:dagger-compiler:${Versions.dagger_version}"
+    const val daggerAndroidCompiler = "com.google.dagger:dagger-android-processor:${Versions.dagger_version}"
 }
 
 object ThirdPartyDependencies {
     const val glide = "com.github.bumptech.glide:glide:${Versions.glide_version}"
+    const val dagger = "com.google.dagger:dagger:${Versions.dagger_version}"
+    const val daggerAndroid = "com.google.dagger:dagger-android:${Versions.dagger_version}"
+    const val daggerAndroidSupport = "com.google.dagger:dagger-android-support:${Versions.dagger_version}"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -11,7 +11,8 @@ object Versions {
     const val lifecycle_ktx_version = "2.2.0"
     const val activity_ktx = "1.2.1"
     const val fragment_ktx = "1.3.1"
-
+    const val dagger_version = "2.34"
+    
     const val gradle_version = "4.1.3"
     const val ktlint_version = "10.0.0"
 
